451c264e16ecc7a4e4f4a80f1171a32cb5aeaf1c
TMWAG <semyon.valyaev@gmail.com>
2023-06-03 20:15:44 +0300

    подключен шрифт source sans pro, добавлен Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { Navbar } from '@/components/layout/Navbar/Navbar';
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Source_Sans_Pro } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const source_sans_pro = Source_Sans_Pro({ subsets: ['latin', 'cyrillic'], style: ['normal'], weight: ['700', '400'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <body className={source_sans_pro.className}>
+        <Navbar/>
+        {children}
+      </body>
     </html>
   )
 }

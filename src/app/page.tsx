59776c2f15f6b54df816db979d278ef70fd46b00
TMWAG <semyon.valyaev@gmail.com>
2023-06-05 20:13:17 +0300

    убраны ненужные компоненты

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { getCategories } from '@/lib/api/getCategories';
-import Categories from './components/Categories/Categories'
-import InfoBlock from './components/InfoBlock/InfoBlock'
 import styles from './page.module.css'
 
 export default async function Home() {
-  const categories = await getCategories();
   return (
     <main className={styles.main}>
-      <InfoBlock/>
-      <Categories categories={categories}/>
+      main
     </main>
   );
 };
